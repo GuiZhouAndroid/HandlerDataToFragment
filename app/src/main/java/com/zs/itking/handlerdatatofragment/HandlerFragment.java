52c0626cbe60aeafa54b5034c149a21d187e9389
GuiZhouAndroid <2393579405@qqcom>
2021-11-03 16:44:18 +0800

    Fragment与Activity使用Handler进行交互

---
+++ app/src/main/java/com/zs/itking/handlerdatatofragment/HandlerFragment.java
@@ -0,0 +1,66 @@
+package com.zs.itking.handlerdatatofragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+/**
+ * created by on 2021/11/3
+ * 描述：接口Handler消息
+ *
+ * @author ZSAndroid
+ * @create 2021-11-03-16:29
+ */
+
+
+public class HandlerFragment extends Fragment {
+
+    private TextView text;
+
+    // 接收MainActivity发送消息，匹配消息what值(消息标记)
+    public Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case 1:
+                    //匹配成功，把object类型的数据强转为String，并设置到TextView控件上
+                    text.setText((String) msg.obj);
+                    break;
+            }
+        };
+    };
+
+    public HandlerFragment() {
+    }
+
+    public static HandlerFragment getInstance() {
+        return new HandlerFragment();
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        MainActivity mActivity = (MainActivity) activity;
+        mActivity.setHandler(mHandler);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_handler, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        text = (TextView) view.findViewById(R.id.text);
+    }
+}
+
---
+++ app/src/main/java/com/zs/itking/handlerdatatofragment/MainActivity.java
@@ -0,0 +1,58 @@
+package com.zs.itking.handlerdatatofragment;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edit;
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        // 1.填充Fragment到FrameLayout控件中
+        getFragmentManager().beginTransaction().replace(R.id.layout_fragment, HandlerFragment.getInstance()).commit();
+        // 2.获取输入框控件
+        edit = (EditText) findViewById(R.id.edit);
+    }
+
+    /**
+     * MainActivity 与 HandlerFragment 共享Handler实例
+     *
+     * @param handler
+     */
+    public void setHandler(Handler handler) {
+        // 3.在HandlerFragment中通过调用此方法，即可实现共享Handler消息对象
+        mHandler = handler;
+    }
+
+    /**
+     * 点击开始传递数据
+     *
+     * @param view
+     */
+    public void star(View view) {
+        //4.创建发送消息实例，在HandlerFragment中接收此消息，即可得到传输的数据信息
+        Message msg = new Message();
+        //5.携带数据为输入框文本数据
+        msg.obj = edit.getText().toString();
+        //5.消息标记为1
+        msg.what = 1;
+        //5.开始发送消息
+        mHandler.sendMessage(msg);
+    }
+}
